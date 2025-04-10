06688a29cab84dda37d68da2cafc0ab753b5b293
emir <emir3710549@gmail.com>
2025-04-10 15:00:06 +0200

    before HMR

---
+++ lib/AppInfo/Application.php
@@ -0,0 +1,25 @@
+<?php
+
+declare(strict_types=1);
+
+namespace OCA\ProjectManagement\AppInfo;
+
+use OCP\AppFramework\App;
+use OCP\AppFramework\Bootstrap\IBootContext;
+use OCP\AppFramework\Bootstrap\IBootstrap;
+use OCP\AppFramework\Bootstrap\IRegistrationContext;
+
+class Application extends App implements IBootstrap {
+	public const APP_ID = 'projectmanagement';
+	public const NOTE_FOLDER_NAME = 'ProjectManagementNotes';
+
+	public function __construct(array $urlParams = []) {
+		parent::__construct(self::APP_ID, $urlParams);
+	}
+
+	public function register(IRegistrationContext $context): void {
+	}
+
+	public function boot(IBootContext $context): void {
+	}
+}
---
+++ src/views/App.vue
@@ -0,0 +1,26 @@
+<template>
+	<NcAppContent>
+		<div id="projectmanagement">
+			<h1>Hello word!</h1>
+		</div>
+	</NcAppContent>
+</template>
+
+<script>
+import NcAppContent from '@nextcloud/vue/dist/Components/NcAppContent.js'
+
+export default {
+	name: 'App',
+	components: {
+		NcAppContent,
+	},
+}
+</script>
+
+<style scoped lang="scss">
+#projectmanagement {
+	display: flex;
+	justify-content: center;
+	margin: 36px;
+}
+</style>
